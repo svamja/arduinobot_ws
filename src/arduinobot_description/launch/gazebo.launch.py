4cd9497a4c1e26862ce3d481b230286a10f451f1
Sanjay Vamja <svamja@gmail.com>
2025-08-03 09:39:34 +0000

    camera assignment
    - add gazebo camerz sensor to URDF

--- src/arduinobot_description/launch/gazebo.launch.py
+++ src/arduinobot_description/launch/gazebo.launch.py
@@ -61,6 +61,8 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/image_raw@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
         ]
     )
 
