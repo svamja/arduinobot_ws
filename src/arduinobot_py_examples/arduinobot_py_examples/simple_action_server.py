bc3c3a7db616f4641ec11d41ac166bbe5e4adb1c
Sanjay Vamja <svamja@gmail.com>
2025-08-21 12:09:43 +0530

    actions demo - completed

---
+++ src/arduinobot_py_examples/arduinobot_py_examples/simple_action_server.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionServer
+from arduinobot_msgs.action import Fibonacci
+import time
+
+class SimpleActionServer(Node):
+    
+    def __init__(self):
+        super().__init__("simple_action_server")
+        self.action_server = ActionServer(self, Fibonacci, "fibonacci", self.goalCallback)
+        self.get_logger().info("Action server 'fibonacci' is ready to receive requests.")
+
+
+    def goalCallback(self, goal_handle):
+        self.get_logger().info("Received goal request.")
+        feedback_msg = Fibonacci.Feedback()
+        feedback_msg.partial_sequence = [0, 1]
+
+        for i in range(1, goal_handle.request.order):
+            feedback_msg.partial_sequence.append(feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i - 1])
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
+
+        goal_handle.succeed()
+        result = Fibonacci.Result()
+        result.sequence = feedback_msg.partial_sequence
+        return result
+
+
+
+def main():
+    rclpy.init()
+    simple_action_server = SimpleActionServer()
+    rclpy.spin(simple_action_server)
+    simple_action_server.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
